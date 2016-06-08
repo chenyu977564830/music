632da27a3fbdd7efc4be1134d98e63f716edb94d
chenyu977564830 <977564830@qq.com>
2016-06-08 21:31:44 +0800

    第四次提交

---
+++ music/index.php
@@ -0,0 +1,6 @@
+<?php
+define('SRC_PATH','http://www.music.com');//定义网站根目录用于资源引入
+define('APP_DEBUG', true);//开启调试模式
+define('APP_PATH','../musicApplication/');//指定项目路径
+require "../ThinkPHP/ThinkPHP.php";//引入核心框架，框架代码与应用代码分离
+?>
